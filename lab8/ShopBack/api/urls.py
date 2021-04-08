c506458d5a465a7afac84e5f70363872fbe50f4c
yrssl01 <62808305+yrssl01@users.noreply.github.com>
2021-04-08 13:00:09 +0500

    Update urls.py

--- lab8/ShopBack/api/urls.py
+++ lab8/ShopBack/api/urls.py
@@ -4,7 +4,7 @@ from api import views
 
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:id>/', views.product_one),
+    path('products/<int:id>/', views.product_),
     path('category/', views.category_list),
-    path('category/<int:id>/', views.category_one),
+    path('category/<int:id>/', views.category_),
 ]
